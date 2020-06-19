a09621320caa14744312174eb619ea8757e300af
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2020-06-19 19:49:24 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,12 +1,7 @@
 class App < Sinatra::Base
 
-<<<<<<< HEAD
-
-  
-=======
-  # Define your GET '/' route below and respond with "Hello, World!"
-
-  get '/' do
+get '/' do
     "Hello, World"
 end
->>>>>>> 8e9f85a0fcba9daed4d9f022aa108cb3cea52069
+
+end
